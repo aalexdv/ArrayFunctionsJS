ae9977a1b1881c58534b62576e7ab787d3fb99d1
aalexdv <123240909+aalexdv@users.noreply.github.com>
2023-02-12 15:49:59 +0100

    Add files via upload

---
+++ ConvertingHEXToRGB.js
@@ -0,0 +1,50 @@
+/*Question 3: Write a function that converts HEX to RGB. Then Make that function auto-dect 
+the formats so that if you enter HEX color 
+format it returns RGB and if you enter RGB color format it returns HEX.*/
+
+function convertColor(color) {
+  var convertedColor;
+
+  if (color[0] === '#') {
+
+    convertedColor = convertHexToRgb(color);
+
+  } else if (color[0] === '(') {
+
+    convertedColor = convertRgbToHex(color);
+  } else {
+
+    return "Error ! \nPlease Enter A Proper Input. \nHex starts with an octothorpe and has 6 trailing numbers, rgb is enclsed in paranthesis and seperated with commas. ";
+  }
+
+  function convertHexToRgb(color) {
+    var hexNumber = color.slice(1);
+    var r = parseInt(hexNumber.substring(0, 2), 16);
+    var g = parseInt(hexNumber.substring(2, 4), 16);
+    var b = parseInt(hexNumber.substring(4, 6), 16);
+    var a = hexNumber.length === 8 ? Math.round(parseInt(hexNumber.substring(6, 8), 16) / 255) : '';
+
+    return "(" + r + "," + g + "," + b + (a !== '' ? ',' : '') + a + ")";
+  }
+
+  function convertRgbToHex(color) {
+    color = color.slice(1, -1).split(',');
+    var r = color[0];
+    var g = color[1];
+    var b = color[2];
+    var a = color.length === 4 ? Math.round(color[3] * 255) : '';
+    return "#" + numberToHex(r) + numberToHex(g) + numberToHex(b) + (color.length === 4 ? numberToHex(a) : '');
+  }
+
+  function numberToHex(number) {
+    var numberInHex = Number(number).toString(16);
+    return numberInHex.length == 1 ? "0" + numberInHex : numberInHex;
+  }
+
+  return convertedColor;
+}
+
+convertColor('#0000FF');
+// '(0,0,255)'
+convertColor('(0, 255, 255)');
+//'#00ffff'
